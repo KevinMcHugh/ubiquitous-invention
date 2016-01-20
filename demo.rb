eff124f28f3e7569198394cb5b805be82af7f4d8
Kevin McHugh <kev@kevinmchugh.me>
2016-01-19 23:02:49 -0600

    Extracts code to relevant models

--- demo.rb
+++ demo.rb
@@ -1,11 +1,19 @@
+module RollsDice
+  def d(int)
+    Random.rand(int) + 1
+  end
+end
 class Person
-  attr_reader :name, :alive
+  include RollsDice
+  attr_reader :name, :alive, :age, :gender, :months_until_birth, :pregnant
   attr_accessor :religion, :health
 
   def initialize
     @name = Faker::Name.name
-    @health = 75 + Random.rand(50)
+    @health = 75 + d(50)
     @alive = true
+    @age = d(70)
+    @gender = d(2) == 1 ? :man : :woman
   end
 
   def alive?
@@ -15,12 +23,29 @@ class Person
   def kill!
     @alive = false
   end
+
+  def age!
+    @age += 1
+    if (age > 45 && health > 10) || age < 80
+      health -= 1
+    else
+      health += 1
+    end
+  end
+
+  def reproduce!
+    if (age > 15 && age < 45) && d(2) == 1 && !pregnant
+      @pregnant = true
+      @months_until_birth = 9
+    end
+  end
 end
 
 class Religion
-  attr_reader :name, :faithful, :policies
-  def initialize(name)
-    @name = name
+  attr_reader :world, :name, :faithful, :policies
+  def initialize(world)
+    @world = world
+    @name = Faker::App.name
     @faithful = 100.times.map { Person.new }
     @faithful.each { |f| f.religion = self }
     @policies = 3.times.map { Policy.new }
@@ -38,6 +63,21 @@ class Religion
 
   def number_of_living_faithful
     faithful.count(&:alive?)
+  end
+
+  def tick
+    if number_of_living_faithful > 0
+      policy = policies.find { |p| p.act == world.active_event.affects }
+      consequence_value = policy && policy.consequence || 100
+      check = (100 - consequence_value)
+      pre = number_of_living_faithful
+      if check < world.active_event.severity
+        faithful.each { |person| person.health -= world.active_event.severity }
+      end
+      faithful.each { |person| person.kill! if person.health <= 0 }
+      post = number_of_living_faithful
+      puts "   The faithful of #{name} number #{post}. #{pre - post} have died in the plague."
+    end
   end
 end
 
@@ -61,37 +101,52 @@ class Policy
   end
 end
 
+class World
+  attr_reader :active_event, :religions, :year
+  def initialize
+    @year = 0
+    @religions = []
+  end
+
+  def add_religion
+    r = Religion.new(self)
+    religions << r
+    r
+  end
+
+  def population
+    religions.map(&:number_of_living_faithful).reduce(:+)
+  end
+
+  def tick
+    @year += 1
+    @active_event = Event.new
+    puts "Year #{year}: A plague strikes those who #{active_event.affects}. Severity is #{active_event.severity}%"
+    religions.each(&:tick)
+  end
+end
+
+class Event
+  include RollsDice
+  attr_reader :affects, :severity
+  def initialize
+    @affects = Policy.acts.sample
+    @severity = d(100)
+  end
+end
+
 require 'faker'
 require 'pp'
 require 'pry'
+world = World.new
 religions = 5.times.map do
-  r = Religion.new(Faker::App.name)
+  r = world.add_religion
   puts r.long_string
   r
 end
 
 100.times do |i|
-  event = Policy.acts.sample
-  difficulty = Random.rand(100)
-  population = religions.map(&:number_of_living_faithful).reduce(:+)
+  break if world.population <= 0
 
-  break if population <= 0
-  puts "Year #{i}: A plague strikes those who #{event}. Severity is #{difficulty}%"
-  religions.each do |r|
-    if r.number_of_living_faithful > 0
-      policy = r.policies.find { |p| p.act == event }
-      consequence_value = policy && policy.consequence || 100
-      check = (100 - consequence_value)
-      pre = r.number_of_living_faithful
-      if check < difficulty
-        r.faithful.each { |person| person.health -= 0.10 * difficulty }
-      end
-      r.faithful.each { |person| person.kill! if person.health <= 0 }
-      post = r.number_of_living_faithful
-      puts "   The faithful of #{r.name} number #{post}. #{pre - post} have died in the plague."
-    end
-  end
+  world.tick
 end
-
-
-
